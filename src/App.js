889f409d1d5c79fbf8c5517d255f5d445fa5aace
raym <a4265622@opayq.com>
2020-03-16 19:27:12 -0700

    Added the JS30 clock code, did the simplest thing that could possibly work

--- src/App.js
+++ src/App.js
@@ -2,24 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+  // const secondHand = document.querySelector('.second-hand');
+  // const minsHand = document.querySelector('.min-hand');
+  // const hourHand = document.querySelector('.hour-hand');
+
+  // function setDate() {
+  //   const now = new Date();
+
+  //   const seconds = now.getSeconds();
+  //   const secondsDegrees = ((seconds / 60) * 360) + 90;
+  //   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  //   const mins = now.getMinutes();
+  //   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  //   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  //   const hour = now.getHours();
+  //   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  //   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  // }
+
+  // setInterval(setDate, 1000);
+
+  // setDate();
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>JS + CSS Clock</title>
+</head>
+<body>
+
+
+    <div class="clock">
+      <div class="clock-face">
+        <div class="hand hour-hand"></div>
+        <div class="hand min-hand"></div>
+        <div class="hand second-hand"></div>
+      </div>
     </div>
+
+</body>
+</html>
   );
 }
 
