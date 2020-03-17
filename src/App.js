a686a72ff8948c01b856a41ee96298698c43abbc
raym <a4265622@opayq.com>
2020-03-16 19:51:07 -0700

    a little cleanup in the JSX

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function App() {
 
   // const secondHand = document.querySelector('.second-hand');
   // const minsHand = document.querySelector('.min-hand');
   // const hourHand = document.querySelector('.hour-hand');
 
-  // function setDate() {
-  //   const now = new Date();
+//   function setDate() {
+//     const now = new Date();
 
-  //   const seconds = now.getSeconds();
-  //   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  //   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+//     const seconds = now.getSeconds();
+//     const secondsDegrees = ((seconds / 60) * 360) + 90;
+//     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  //   const mins = now.getMinutes();
-  //   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  //   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+//     const mins = now.getMinutes();
+//     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+//     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-  //   const hour = now.getHours();
-  //   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  //   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-  // }
+//     const hour = now.getHours();
+//     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+//     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+//   }
 
-  // setInterval(setDate, 1000);
+//   setInterval(setDate, 1000);
 
-  // setDate();
+//   setDate();
 
-function App() {
   return (
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>JS + CSS Clock</title>
-</head>
-<body>
-
-
     <div class="clock">
       <div class="clock-face">
         <div class="hand hour-hand"></div>
@@ -43,9 +35,6 @@ function App() {
         <div class="hand second-hand"></div>
       </div>
     </div>
-
-</body>
-</html>
   );
 }
 
